3434b84eb331fe18fe1d68ccfd3a24eff9988405
Jumner <justin462@protonmail.com>
2025-09-18 06:55:44 -0400

    Overall stress uses start date

--- src/list/stress.rs
+++ src/list/stress.rs
@@ -2,6 +2,14 @@ use super::List;
 
 impl List {
     pub fn total_stress(&self) -> f32 {
-        self.tasks.keys().map(|&id| self.stress(id)).sum()
+        self.tasks
+            .keys()
+            .filter_map(|&id| {
+                if self.tasks.get(&id).unwrap().started() {
+                    return Some(self.stress(id));
+                }
+                return None;
+            })
+            .sum()
     }
 }
